2bac9aea3c18673b65e6d72d3597dafee29a64d2
Octavio Becerril Olivares <136529745+MFei8ht@users.noreply.github.com>
2023-09-07 09:46:41 -0500

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,19 @@
+package operaciones;
+public class Practica2 {
+    public static void main(String[] args) {
+        String nombre="OCTAVIO BECERRIL OLIVARES";
+        System.out.println(nombre);
+        System.out.println("Este programa calculara el area de un rectangulo");
+        double base=10.2;
+        double altura=5.2;
+        double resultado=base*altura;
+        System.out.println("El area del rectangulo con decimales es: "+resultado);
+        
+        int baseInt=10;
+        int alturaInt=5;
+        int resultadoInt=baseInt*alturaInt;
+        System.out.println("El area del rectangulo con enteros es: "+resultadoInt);
+
+    }
+    
+}
